c75c73df7e0beb757c20b1b09d949cc75ae8f5a6
Mounika Pydi <mounikapydi01@gmail.com>
2019-09-20 07:58:48 +0530

    user management

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import DisplayUsers from './displayUsers';
+import DisplayUserInfo from './displayUserInfo';
+import Search from './Search';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Router>
+          <Switch>
+            <Route path={'/:id'} component={DisplayUserInfo}/>
+            <Route path={'/'} component={DisplayUsers} />
+          </Switch>
+        </Router>
+      </div>
   );
 }
 
